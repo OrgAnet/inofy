924f072923f0302d597af719f60e3511b273ccc1
ozanmuyes <ozanmuyes@gmail.com>
2017-03-08 22:24:51 +0300

    Add CLI arguments support

--- src/main/java/org/organet/inofy/App.java
+++ src/main/java/org/organet/inofy/App.java
@@ -1,15 +1,38 @@
 package org.organet.inofy;
 
+import org.apache.commons.cli.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 
 public class App {
-  private static final String SHARED_DIR_PATH = "/home/ozan/organet_shared";
+  private static String sharedDirPath = "/home/ozan/organet_shared";
   private static File sharedDir;
 
   public static void main(String[] args) {
-    sharedDir = new File(SHARED_DIR_PATH);
+    Options options = new Options();
+
+    options.addOption("S", "shared-dir", true, "Absolute directory path to shared files and folders.");
+    // TODO Add CLI arguments here
+
+    CommandLineParser parser = new GnuParser();
+    CommandLine cmd;
+    try {
+      cmd = parser.parse(options, args);
+    } catch (ParseException e) {
+      System.out.println("Could not parse the commandline arguments. Aborting.");
+
+      return;
+    }
+
+    // Set global variables by CLI arguments
+    if (cmd.hasOption('S')) {
+      sharedDirPath = cmd.getOptionValue('S');
+    }
+
+    // Update variables which depends CLI arguments
+    sharedDir = new File(sharedDirPath);
 
     if (!sharedDir.isDirectory()) {
       System.out.println("Error: OrgAnet shared directory was not set properly. Aborting.");
